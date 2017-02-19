5fe22b786e47878f27b9cc3f707a09e6802c2cd2
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-02-19 12:15:36 -0300

    fix delete method

--- src/controllers/expedicao.js
+++ src/controllers/expedicao.js
@@ -5,32 +5,32 @@ class ExpedicaoController {
 
   getAll(req, res) {
     return this.Expedicao.find({})
-    .then(expedicoes => res.send(expedicoes))
-    .catch(err => res.status(400).send(err.message));
+      .then(expedicoes => res.send(expedicoes))
+      .catch(err => res.status(400).send(err.message));
   }
 
   getById(req, res) {
     return this.Expedicao.findById(req.params.id)
-    .then(expedicao => res.send(expedicao))
-    .catch(err => res.status(400).send(err.message));
+      .then(expedicao => res.send(expedicao))
+      .catch(err => res.status(400).send(err.message));
   }
 
   create(req, res) {
     return this.Expedicao.create(req.body)
-    .then(expedicao => res.status(201).send(expedicao))
-    .catch(err => res.status(412).send(err.message));
+      .then(expedicao => res.status(201).send(expedicao))
+      .catch(err => res.status(412).send(err.message));
   }
 
   update(req, res) {
     return this.Expedicao.update(req.params.id, req.body)
-    .then(rowsUpdated => res.send(rowsUpdated))
-    .catch(err => res.status(412).send(err.message));
+      .then(rowsUpdated => res.send(rowsUpdated))
+      .catch(err => res.status(412).send(err.message));
   }
 
   delete(req, res) {
-    return this.Expedicao.update(req.params.id)
-    .then(() => res.sendStatus(204))
-    .catch(err => res.status(400).send(err.message));
+    return this.Expedicao.remove({ _id: req.params.id })
+      .then(() => res.sendStatus(204))
+      .catch(err => res.status(400).send(err.message));
   }
 }
 
