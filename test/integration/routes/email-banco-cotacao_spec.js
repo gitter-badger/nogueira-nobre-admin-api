f8e91335eddfb357b2777af9e8e6e7ae26846d64
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-09-12 22:09:03 -0300

    Adicionar DS_Store ao gitignore, refatorar testes da rota email-banco-cotacao

--- test/integration/routes/email-banco-cotacao_spec.js
+++ test/integration/routes/email-banco-cotacao_spec.js
@@ -7,23 +7,17 @@ describe('Route: EmailBancoCotacao', () => {
       msg: 'viiiiila!!',
     };
 
-    it('Should send an email from nao-responda@bancodecotacao.com.br to contato@bancodecotacao.com.br', (done) => {
+    it('Should send an email from nao-responda@bancodecotacao.com.br to contato@bancodecotacao.com.br', () => {
       request
         .post('/api/v1/email/bancocotacao/contato')
         .send(defaultContato)
-        .end((err, res) => {
-          expect(res.status).to.be.eql(200);
-          done();
-        });
+        .expect(200);
     });
 
-    it('Should receive a status code 412 when an email don\'t have a message, name, email', (done) => {
+    it('Should receive a status code 412 when an email don\'t have a message, name, email', () => {
       request
         .post('/api/v1/email/bancocotacao/contato')
-        .end((err, res) => {
-          expect(res.status).to.be.eql(412);
-          done();
-        });
+        .expect(412);
     });
   });
 
@@ -33,23 +27,17 @@ describe('Route: EmailBancoCotacao', () => {
       file: 'planilha.xls',
     };
 
-    it('Should send an email to nao-responda@bancodecotacao.com.br to cotacao@bancodecotacao.com.br', (done) => {
+    it('Should send an email to nao-responda@bancodecotacao.com.br to cotacao@bancodecotacao.com.br', () => {
       request
-       .post('/api/v1/email/bancocotacao/cotacao')
-       .send(defaultCotacao)
-       .end((err, res) => {
-         expect(res.status).to.be.eql(200);
-         done();
-       });
+        .post('/api/v1/email/bancocotacao/cotacao')
+        .send(defaultCotacao)
+        .expect(200);
     });
 
-    it('Should send an email to nao-responda@bancodecotacao.com.br to cotacao@bancodecotacao.com.br', (done) => {
+    it('Should receive a status code 412 when an cotacao don\'t have the required parameters', () => {
       request
-       .post('/api/v1/email/bancocotacao/cotacao')
-       .end((err, res) => {
-         expect(res.status).to.be.eql(412);
-         done();
-       });
+        .post('/api/v1/email/bancocotacao/cotacao')
+        .expect(412);
     });
   });
 });
