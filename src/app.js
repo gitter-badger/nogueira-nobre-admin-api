6c4dea1a3d0e9870eb12320fe6d52a68d55b0fb8
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-10-16 19:53:23 -0200

    Corrigir erros de lint

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ database.once('open', () => {
   console.log(`connected to  ${process.env.MONGODB_URL || 'mongodb://localhost:27017/nogueira-nobre'}`);
   app.use(bodyParser.json());
   app.use(initializeAuth());
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
   app.use('/', routes);
