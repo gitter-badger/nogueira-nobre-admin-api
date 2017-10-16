217fc7496ef551798cd78855a8f682f446469021
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-10-16 19:50:01 -0200

    Adicionar rota de envio de email para Nogueira Nobre

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import expedicaoRoute from './expedicao';
 import userRoute from './user';
 import authRotue from './auth';
 import emailBancoCotacaoRoute from './email-banco-cotacao';
+import emailNogueiraNobre from './email-nogueira-nobre'
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/api/v1/auth', authRotue);
 router.use('/api/v1/expedicoes', expedicaoRoute);
 router.use('/api/v1/users', userRoute);
 router.use('/api/v1/email/bancocotacao', emailBancoCotacaoRoute);
+router.use('/api/v1/email/nogueira-nobre', emailNogueiraNobre);
 
 export default router;
