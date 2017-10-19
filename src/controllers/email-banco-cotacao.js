979e06e2a47327ae4e3f244d86d59f16b05d1b90
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-10-19 10:50:46 -0200

    Fix aws config path part 2

--- src/controllers/email-banco-cotacao.js
+++ src/controllers/email-banco-cotacao.js
@@ -60,7 +60,7 @@ function sendEmailBancoCotacaoCotacao(req, res) {
       });
   } else {
     // configure AWS SDK
-    AWS.config.loadFromPath(path.resolve(`${__dirname}/../../aws.config.json`));
+    AWS.config.loadFromPath(path.resolve('aws.config.json'));
     // create Nodemailer SES transporter
     const transporter = nodemailer.createTransport({
       SES: new AWS.SES({
